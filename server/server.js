dab686a0ce7688285b2fae1e7655dd02bddc4714
Jarred Baca <jarredbaca@MacBook-Pro.local>
2020-09-27 22:32:42 -0600

    finished module 3

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -11,7 +13,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
@@ -19,6 +22,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
